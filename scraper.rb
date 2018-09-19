de8ea6e5e91a1e609594680be6c4669c47bdf046
sspatari <spatari.stanislav@gmail.com>
2018-09-19 08:08:48 +0300

    Acces demo account

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,6 @@ url = "https://my.fibank.bg/oauth2-server/login?client_id=E_BANK"
 browser = Watir::Browser.new(:firefox)
 
 browser.goto(url)
-
-puts browser.title
+browser.link(:id => "demo-link").click
 
 browser.close
