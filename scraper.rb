4c171cbebf14a7ccc17c9653eff5732e5ca25a63
sspatari <spatari.stanislav@gmail.com>
2018-09-20 14:31:56 +0300

    Print account details

--- scraper.rb
+++ scraper.rb
@@ -8,4 +8,15 @@ browser.link(:id => "demo-link").click
 browser.ul(:id => "sidebar").li.wait_until_present
 browser.ul(:id => "sidebar").link("ui-sref" => "app.layout.ACCOUNTS").click
 
+browser.div(:class => ["box-border", "ng-scope"]).wait_until_present
+
+browser.elements(:class => ["box-border", "ng-scope"]).each do |item|
+  puts "**Account**:"
+  puts "-name " + item.div(:index => 0).span.text
+  puts "-currency " + item.div(:index => 1).span.text
+  puts "-balance " + item.td.text
+  puts "-nature " + item.div(:index => 0).h5.text
+  puts
+end
+
 browser.close
